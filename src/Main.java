5b905b80d591e7a3a92a5955b3c04d5e19033392
micha <michaelrizig@gmail.com>
2023-06-26 18:11:36 -0400

    update 1; added group, groupleader, user, password , and exceptions for various issues. update 2; added Platform class, further implimented password, created one of 3 encrypt methods and verified working order update 3: completed and tested encrypt method 0 and 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[]args)
     {
-        Password one = new Password("password");
+        Password one = new Password("password\\4845");
 
     }
 }
--- src/Password.java
+++ src/Password.java
@@ -3,25 +3,22 @@ import java.util.Random;
 public class Password {
     private String encryptedPass;
     private int encryptMethodID;
+
+    //PASSWORDS CAN NOT INCLUDE '#' or '&' symbols in sequence.
     Password(String originalPass)
     {   Random rand = new Random();
-        encryptMethodID = rand.nextInt(4);
-        System.out.println("[debug in password line 10] rand variable is " + rand.nextInt(4) );
-        String a = encryptMethod0(originalPass);
-        System.out.println(a);
-        System.out.println(decryptMethod0(a));
+        encryptMethodID = rand.nextInt(3);
         switch (encryptMethodID)
         {
             case 0:
                 encryptedPass = encryptMethod0(originalPass);
-                encryptMethodID = 0;
                 break;
             case 1:
+                encryptedPass = encryptMethod1(originalPass);
                 break;
             case 2:
                 break;
-            case 3:
-                break;
+
         }
 
     }
@@ -34,7 +31,7 @@ public class Password {
 
             encryptedPass += index + Integer.toHexString( ((int) originalPass.charAt(j) ) ^ index )+"-";
             //converts into a line of 3 digits for each char by using hexStrings; adds a '-' symbol between each set of 3
-            System.out.println("[debug in Passowrd line 36] current epass text is " + encryptedPass);
+            //System.out.println("[debug in Passowrd line 36] current epass text is " + encryptedPass);
         }
         return encryptedPass.substring(0, encryptedPass.lastIndexOf("-"));
     }
@@ -53,6 +50,50 @@ public class Password {
             //reverses encryption of prev method, returns decrypted pass
         }
         return decryptedPass;
+    }
+
+    private String encryptMethod1(String originalPass)
+    {   //WHEN
+        Random rand = new Random();
+        int key = rand.nextInt(10);
+        //creates random single digit key variable which will be necessary to decode the password
+        String [] array = originalPass.split("");
+        //splits string into array
+        String encryptedPass = "" + (key+54);
+        for(int i=7;i<array.length+7;i++)
+        {
+          encryptedPass+="#@";
+            //adds delimiting characters
+            encryptedPass+= (i + array[i-7].charAt(0) + key );
+            //changes char value of each character by current i value (7+) and a randomized key variable, which is hidden within in the encrypted password;
+            //System.out.println("[debug line 72] current password in run " + i +  " is " + encryptedPass);
+
+        }
+        return encryptedPass;
+    }
+    private String decryptMethod1(String encryptedPass)
+    {
+        String decryptedPass= "";
+
+        int key = Integer.parseInt(encryptedPass.substring(0,2)) - 54;
+        //retreives the key value from the encrypted pass by taking the first term before delimiter and subtracting 54
+        //System.out.println("[debug]derived key is "+key);
+
+        String [] array = encryptedPass.substring(4).split("#@");
+        //creates array of each char value excluding the 2 digit key(which is randomized single digit key from line 64 plus the arbitrary int 54) and first delimiter
+        //System.out.println("[debug line 82, passed password is " + encryptedPass);
+
+        for(int i=7;i<array.length+7;i++)
+        {    array[i-7]= array[i-7].replace("#@","");
+            //removing delimiters
+            char a = (char)(Integer.parseInt(array[i-7])-i - key);
+            //converts back into letters
+            decryptedPass+=a;
+            //rebuilds the string
+
+        }
+        return decryptedPass;
+
     }
 
 }
