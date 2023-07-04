1c60bf18138b445f7ded34c5b67dc85bd163a0d3
micha <michaelrizig@gmail.com>
2023-07-04 19:56:13 -0400

    completed login page

--- src/testUI.java
+++ src/testUI.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.geom.RoundRectangle2D;
 
 public class testUI {
     //test username is user; password is pass
@@ -30,13 +29,16 @@ public class testUI {
     public JLabel loginFrame = new JLabel();
     LineBorder roundedLineBorder = new LineBorder(Color.GRAY, 2, true);
     TitledBorder roundedTitledBorder = new TitledBorder(roundedLineBorder, "");
-    JTextField username = new JTextField("Username");
+    JTextField username = new JTextField(" Username");
     JPasswordField password = new JPasswordField();
     JButton loginButton = new JButton("Login");
+    JLabel or = new JLabel("- - - - - or - - - - -");
+    JButton createNewAccountButton = new JButton();
 
 
     testUI(GroupManager groupManager)
     {   this.groupManager=groupManager;
+        initialize();
         frame.setSize(400,800);
         frame.setLocationRelativeTo(null);
         frame.add(panel);
@@ -57,7 +59,7 @@ public class testUI {
 
         login.setVisible(true);
         login.setSize(200,90);
-        login.setLocation(90,200);
+        login.setLocation(90,210);
         //login.setHorizontalAlignment(JLabel.CENTER);
         login.setFont(new Font("Tahoma", Font.BOLD, 20));
 
@@ -86,7 +88,7 @@ public class testUI {
         password.setBounds(120,360,150,30);
         password.setVisible(true);
         password.setEchoChar(c);
-        password.setText("Password");
+        password.setText(" Password");
         password.setBorder(roundedTitledBorder);
         password.addFocusListener(
                 new FocusListener() {
@@ -100,14 +102,16 @@ public class testUI {
                     {
                         if(password.getPassword() == (empty))
                         {   password.setEchoChar(c);
-                            password.setText("Password");
+                            password.setText(" Password");
 
                         }
                     }
                 });
         loginButton.setBorder(roundedTitledBorder);
-        loginButton.setBounds(99,420,200,50);
+        loginButton.setBounds(95,420,200,50);
         loginButton.setBackground(primaryColor);
+        loginButton.setFocusPainted(false);
+        loginButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
 
         loginButton.addActionListener(new ActionListener() {
             @Override
@@ -125,7 +129,29 @@ public class testUI {
             }
         });
 
+        or.setBounds(119, 580, 400,30);
+        or.setVisible(true);
+        or.setFont(new Font("Tahoma", Font.ITALIC, 18));
 
+        createNewAccountButton.setBorder(roundedTitledBorder);
+        createNewAccountButton.setBounds(95,640,200,50);
+        createNewAccountButton.setBackground(new Color(144,140,140));
+        createNewAccountButton.setVisible(true);
+        createNewAccountButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        createNewAccountButton.setText("Create New Account");
+        createNewAccountButton.setFocusPainted(false);
+
+
+
+
+    }
+    public void closeLogin(){}
+    public void homepage(){};
+
+    public void initialize()
+    {
+        panel.add(createNewAccountButton);
+        panel.add(or);
         panel.add(username);
         panel.add(password);
         panel.add(loginButton);
@@ -133,12 +159,7 @@ public class testUI {
         panel.add(title);
         panel.add(loginFrame);
 
-
     }
-    public void closeLogin(){}
-    public void homepage(){};
-
-
     public void drawGroups()
     {   int start =200;
         for (Group i : groupManager.groups)
